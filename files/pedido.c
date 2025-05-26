374c60ee351a39378067a97da3e813889b0512a2
Ian-S1lva <ian.cruz.074@ufrn.edu.br>
2025-05-26 16:41:39 +0000

    pedidos

--- files/pedido.c
+++ files/pedido.c
@@ -1,20 +1,124 @@
 //implementacao do codigo do salao
 #include <stdio.h>
+#include <string.h>
 #include "pedido.h"
+
+//uma lista contera todos os pratos possiveis o que cada variavel *prato aponta, é para os nomes dessa lista
+
+Pedido pedido_salao;
+
+void inicializar_pedido(Pedido* p) {
+    p->frente = NULL;
+    p->tras = NULL;
+    p->qtd_pratos = 0;
+    //adicionar id com base na qtd de pedidos na cozinha
+}
+
+//verifica se esta vazio
+int pedido_vazio(Pedido* p) {
+    return p->frente == NULL;
+}
+
+void inserir(Pedido* p, char* prato) {
+    No2* novo = (No2*) malloc(sizeof(No2));
+    novo->prato = prato; //prato aponta para o prato a ser inserido
+    novo->prox = NULL;
+
+    // verifica se a fila tá vazia, se tiver, ele é o primeiro
+    if (p->tras == NULL) {
+        p->frente = novo;
+        p->tras = novo;
+    } else {
+        // se não ta vazia, vai p fim
+        p->tras->prox = novo;
+        p->tras = novo;
+    }
+    p->qtd_pratos++;
+}
+
+//busca a ocorrencia de um prato no pedido e retorna sua posicao
+int busca(Pedido* p, char* prato){
+    if (fila_vazia(&pedido_salao)) return -1; //se a fila estiver vazia volte
+    
+    int i = 0;
+    while (atual != NULL) { //
+        if(atual->prato == prato){//essa construcao compila?
+            return i;
+        }
+        atual = atual->prox;
+        i++;
+        
+    }
+    return -1;
+}
+
+//busca e remove a 1 ocorrencia de um prato
+void remover(Pedido* p, char* prato) {
+   
+    if (pedido_vazio(p)) return;
+
+    int prato_na_lista = busca(p,prato);
+    if(prato_na_lista == -1) return;
+
+    if(prato_na_lista == 0){ //trata o caso pro primeiro da lista
+        No2* temp = p->frente;
+        p->frente = p->frente->prox;
+        free(temp);
+        return;
+        }
+    
+        //trata o caso para posicao intermediaria ou final
+
+    No2* atual = p->frente;
+    for (int i = 0; i < prato_na_lista - 1 && atual->prox != NULL; i++) { //busca ponteiro anterior ao nó a ser removido
+        atual = atual->prox;
+    }
+
+    No2* temp = atual->prox;
+    atual->prox = temp->prox;
+    free(temp);
+}
+
+
+  
+   
+
 
 
 void adicionar_prato(){
     //em construcao
+
+    //ler nome
+    //buscar em lista de pratos
+    //se o nome existir
+    //passar o ponteiro q aponta para lista de pratos em inserir()
 } 
 
 void remover_prato(){
     //em construcao
+
+    //ler nome
+    //passar o nome para remover();
+    //caso remover() retorne -1 informar que nao foi possivel remover o prato
 }
 
 void exibir_pedido(){
-    //em construcao
+       if (pedido_vazio(&pedido_salao)) {
+        printf("Nenhum prato adicionado ainda.\n");
+        return;
+    }
+
+    printf("Pedido:\n");
+    No2* atual = pedido_salao.frente;
+    while (atual != NULL) {
+        printf(" - %s\n", atual->prato);
+        atual = atual->prox;
+     
+    }
 } 
 
 void finalizar_pedido(){
     //em construcao
+    //transferir para lista da cozinha
+    //id++
 } 
--- files/pedido.h
+++ files/pedido.h
@@ -1,12 +1,58 @@
 //implementacao do codigo do salao
+#ifndef PEDIDOS_H
+#define PEDIDOS_H
 
-typedef struct{
- 
-} Fila_ligada;
+/*
+
+struct
+
+prato
+
+no -> id prato e prox
+
+fila ligada -> inicio e fim da fila
+
+[yep] [slide 14b, fala de remocoes no meio;fazer insercoes sempre no fim, como uma comanda]
+[remocao da fila, fazer 'rmv lasanha' 1 por vez ou 'rmv lasanha qtd' <-- depende de como implementar a insercao]
+
+*/
+
+#define TAM_NOME 100
+
+typedef struct {
+    int id;
+    int qtd_pratos;
+
+    No2* frente;
+    No2* tras;
+} Pedido; //Fila ligada
+
+typedef struct No2 {
+    char *prato;
+    struct No2* prox;
+} No2;
+
+
 
 //funcoes...
+
+
+void inicializar_pedido(Pedido* p);
+int pedido_vazio(Pedido* p);
+void inserir(Pedido* p, char* prato);
 
 void adicionar_prato();
 void remover_prato();
 void exibir_pedido();
 void finalizar_pedido();
+
+/* vai precisar das funcoes da lista de pedidos
+
+void inicializar_fila(Fila* f); //inicia
+
+void enfileirar(Fila* f, Pedido p); //add pedido
+
+na logica de brunno os pedidos tem um array de pratos e uma qtd de pratos
+pedido.qtd_pratos, pedido.pratos[i], pedido.id
+*/
+#endif
